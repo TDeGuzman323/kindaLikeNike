87ffe5dd710b169ab7331bcf90f407a438edcfb4
Tara DeGuzman <tara.deguzman323@gmail.com>
2019-10-31 20:25:56 -0700

    debugging

--- java.js
+++ java.js
@@ -6,30 +6,30 @@ function openTab(tabName) {
 
   //modal//
   
-  Get the modal
+  //Get the modal
   var modal = document.getElementById("myModal");
   ​
-  Get the button that opens the modal
+  //Get the button that opens the modal
  var btn = document.getElementById("myBtn");
   ​
-  Get the <span> element that closes the modal
+  //Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
   ​
-  When the user clicks the button, open the modal 
+  //When the user clicks the button, open the modal 
  $("#myBtn").click(function() {
    modal.style.display = "block";
   })
   ​
-  When the user clicks on <span> (x), close the modal
+  //When the user clicks on <span> (x), close the modal
   $("span").click(function() {
   modal.style.display = "none";
  })
   ​
-  When the user clicks anywhere outside of the modal, close it
+  //When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
    if (event.target == modal) {
      modal.style.display = "none";
    }
   }
  
-  localStorage.setItem('storeObj', JSON.stringify(modal));. 
+  localStorage.setItem('storeObj', JSON.stringify(modal));
